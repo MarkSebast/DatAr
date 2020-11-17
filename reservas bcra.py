cbc15e909e7ba9b3ce055340929107ae806891e9
Sebastián Mark <sepomark12@gmail.com>
2020-11-17 20:31:01 -0300

    Update reservas bcra.py

--- reservas bcra.py
+++ reservas bcra.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[16]:
+# In[155]:
 
 
 import matplotlib.pyplot as plt
@@ -12,7 +12,7 @@ import numpy as np
 import plotly.io as pio
 
 
-# In[17]:
+# In[2]:
 
 
 colnames = [
@@ -21,14 +21,14 @@ colnames = [
 dat = pandas.read_csv('reservas bcra.csv', names=colnames, sep=';', thousands=".")
 
 
-# In[18]:
+# In[3]:
 
 
 #Elimno primera fila
 data = dat.drop(dat.index[0])
 
 
-# In[19]:
+# In[4]:
 
 
 día = []
@@ -39,13 +39,13 @@ while tot != 933:
 data.insert(2, "día", día)
 
 
-# In[20]:
+# In[5]:
 
 
 data["saldos"] = data["saldos"].str.replace(",","").astype(int)
 
 
-# In[21]:
+# In[6]:
 
 
 #Convirtiendo meses
@@ -64,78 +64,119 @@ data.loc[data['mes'] == 'noviembre', 'mes'] = 11
 data.loc[data['mes'] == 'diciembre', 'mes'] = 12
 
 
-# In[22]:
+# In[7]:
 
 
 df = data['día'].map(str) + '-' + data['mes'].map(str) + '-' + data['año'].map(str)
 
 
-# In[23]:
+# In[8]:
 
 
 df2 = pandas.concat([df, data], axis=1, sort=False)
 
 
-# In[24]:
+# In[9]:
 
 
 df2.head()
 
 
-# In[25]:
+# In[10]:
 
 
 pandas.to_datetime(df2[0])
 
 
-# In[26]:
+# In[11]:
 
 
-df2.rename(columns = {0:'fecha'}, inplace = True) 
+df2.rename(columns = {0:'date'}, inplace = True) 
 
 
-# In[27]:
+# In[14]:
+
+
+df2['date'] = pandas.to_datetime(df2['date'])
+df2.date.dt.strftime('%Y%-m%-d')
+
+
+# In[15]:
 
 
 df2.tail()
 
 
-# In[28]:
+# In[150]:
 
 
-x=df2["fecha"]
-y=df2["saldos"]
+fig = go.Figure()
 
-fig = px.line(df2, x, y,
-              title='Reservas BCRA'
-             )
-fig.update_xaxes(
-    rangeslider_visible=True,
+fig.add_trace(
+   go.Scatter(x=list(df2.date), 
+              y=list(df2.saldos), 
+              name="reservas",
+                line=dict(color='green', width=1),
+                marker={"size": 3},
+                mode="lines+markers",
+                showlegend=False,
+                 )
 )
+
+
+
+
+#Título
+fig.update_layout(
+    title_text="Reservas brutas BCRA por mes"
+)
+
+
+
+#RangeSlider and selector
+fig.update_xaxes(
+    rangeselector=dict(
+        buttons=list([
+            dict(count=6, label="6m", step="month", stepmode="todate"),
+            dict(count=1, label="1y", step="year", stepmode="todate"),
+            dict(count=10, label="10y", step="year", stepmode="todate"),
+        ])),
+    
+    rangeslider=dict(visible=True,
+                    range=["1940-01-28", "2020-08-28"])
+)
+
+fig.update_xaxes(range=["1940-01-28", "2020-08-28"])
+     
+
+#Color range selector
+fig.update_xaxes(rangeselector_bgcolor='gray')
+
+
+#Color de fondo
+fig.update_layout(
+    dragmode="pan",
+    hovermode="x",
+    legend=dict(traceorder="reversed"),
+    height=700,
+    template="plotly_dark",
+    margin=dict(
+        t=100,
+        b=100
+    )
+)
+
+    
+#
+
 
 fig.show()
 
-
-# In[29]:
-
-
-pio.write_html(fig, file='figure.html', auto_open=True)
+#When using "all" button, range changes to dates out of the df. Can't find a solution to that
 
 
-# In[ ]:
+# In[156]:
 
 
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
+pio.write_html(fig, file='reservas bcra.html', auto_open=True)
 
